f63ce0389104d263ac2a0cd38048dac577934ce2
dario-bastos-dev <dario.bastos.dev@gmail.com>
2026-04-28 23:51:01 -0300

    Enviando novas atividades de fila

--- Atv_1/core.h
+++
@@ -1,7 +0,0 @@
-#ifndef CORE_H
-#define CORE_H
-
-int cadastrar(void *caixa, int tamanho, char *arquivo);
-void exibir(char *arquivo, int tamanho, void (*imprimir)(void*));
-
-#endif
--- Atv_3/compactacao_ordenacao/utilidades/utilidades.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILIDADES_H
-#define UTILIDADES_H
-
-void limpar_tela();
-void pausar();
-
-#endif
